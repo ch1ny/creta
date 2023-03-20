371fc1deb74599006bbaf8105fef47d68e628695
德布罗煜 <1056317718@qq.com>
2023-03-20 14:31:28 +0800

    feat: 修改renderer d.ts

--- packages/create-creta/templates/default/src/renderer/global.d.ts
+++ packages/create-creta/templates/default/src/renderer/global.d.ts
@@ -1,31 +1,35 @@
-declare module '*.module.css' {
-	const styles: { readonly [key: string]: string };
-	export default styles;
-}
-declare module '*.module.less' {
-	const styles: { readonly [key: string]: string };
-	export default styles;
-}
-declare module '*.module.scss' {
-	const styles: { readonly [key: string]: string };
-	export default styles;
-}
+import type { IpcRendererEvent } from 'electron';
 
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.gif';
+declare global {
+	declare module '*.module.css' {
+		const styles: { readonly [key: string]: string };
+		export default styles;
+	}
+	declare module '*.module.less' {
+		const styles: { readonly [key: string]: string };
+		export default styles;
+	}
+	declare module '*.module.scss' {
+		const styles: { readonly [key: string]: string };
+		export default styles;
+	}
 
-declare interface Window {
-	ipc: {
-		on: (channel: string, cb: Function) => void;
-		once: (channel: string, cb: Function) => void;
-		removeListener: (channel: string, cb: Function) => void;
-		removeAllListeners: (channel: string) => void;
-		send: (channel: string, ...args: any[]) => void;
-		invoke: (channel: string, ...args: any[]) => Promise<any>;
-		sendSync: (channel: string, ...args: any[]) => void;
-		postMessage: (channel: string, message: any, transfer?: MessagePort[]) => void;
-		sendTo: (webContentsId: number, channel: string, ...args: any[]) => void;
-		sendToHost: (channel: string, ...args: any[]) => void;
-	};
+	declare module '*.png';
+	declare module '*.jpg';
+	declare module '*.gif';
+
+	declare interface Window {
+		ipc: {
+			on: (channel: string, cb: (evt: IpcRendererEvent, ...args: any[]) => any) => void;
+			once: (channel: string, cb: (evt: IpcRendererEvent, ...args: any[]) => any) => void;
+			removeListener: (channel: string, cb: (...args: any[]) => any) => void;
+			removeAllListeners: (channel: string) => void;
+			send: (channel: string, ...args: any[]) => void;
+			invoke: (channel: string, ...args: any[]) => Promise<any>;
+			sendSync: (channel: string, ...args: any[]) => void;
+			postMessage: (channel: string, message: any, transfer?: MessagePort[]) => void;
+			sendTo: (webContentsId: number, channel: string, ...args: any[]) => void;
+			sendToHost: (channel: string, ...args: any[]) => void;
+		};
+	}
 }
